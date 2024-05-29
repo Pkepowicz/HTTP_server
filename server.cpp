a613dbb68f1a544f04673e5115ac0f6052dd0f1a
pkepowicz <paw.kepowicz@gmail.com>
2024-05-29 20:53:16 +0200

    Added server source file

---
+++ server.cpp
@@ -0,0 +1,162 @@
+// Server side C program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/sendfile.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+
+#define PORT 8081
+
+char* parse_method(char line[], const char symbol[]);
+int send_message(int fd, char image_path[], char head[]);
+
+
+char http_header[25] = "HTTP/1.1 200 Ok\r\n";
+
+
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, pid; 
+    long valread;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+    
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("In sockets");
+        exit(EXIT_FAILURE);
+    }
+    
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+    
+    memset(address.sin_zero, '\0', sizeof address.sin_zero);
+    
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
+    {
+        perror("In bind");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 10) < 0)
+    {
+        perror("In listen");
+        exit(EXIT_FAILURE);
+    }
+    
+    
+    while(1)
+    {
+        printf("\n+++++++ Waiting for new connection ++++++++\n\n");
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+        {
+            perror("In accept");
+            exit(EXIT_FAILURE);
+        }
+        //Create child process to handle request from different client
+        pid = fork();
+        if(pid < 0){
+            perror("Error on fork");
+            exit(EXIT_FAILURE);
+        }
+        
+        if(pid == 0){
+            char buffer[30000] = {0};
+            valread = read(new_socket , buffer, 30000);
+
+            printf("\n buffer message: %s \n ", buffer);
+            char *parse_string_method = parse_method(buffer, " ");  //Try to get the path which the client ask for
+            printf("Client method: %s\n", parse_string_method);
+
+
+            char *copy_head = (char *)malloc(strlen(http_header) +200);
+            strcpy(copy_head, http_header);
+
+            if(parse_string_method[0] == 'G' && parse_string_method[1] == 'E' && parse_string_method[2] == 'T'){
+                // GET
+                char path_head[500] = ".";
+                strcat(path_head, "/index.html");
+                strcat(copy_head, "Content-Type: text/html\r\n\r\n");
+                send_message(new_socket, path_head, copy_head);               
+            }
+            else {
+                // OTHER METHODS
+                send_message(new_socket, "./error.html", copy_head);
+            }
+            close(new_socket);
+            free(copy_head);  
+        }
+        else{
+            printf(">>>>>>>>>>Parent create child with pid: %d <<<<<<<<<", pid);
+            close(new_socket);
+        }
+    }
+    close(server_fd);
+    return 0;
+}
+
+
+char* parse_method(char line[], const char symbol[])
+{
+    char *copy = (char *)malloc(strlen(line) + 1);
+    strcpy(copy, line);
+        
+    char *message;
+    char * token = strtok(copy, symbol);
+    int current = 0;
+
+    while( token != NULL ) {
+      
+      if(current == 0){
+          message = token;
+          if(message == NULL){
+              message = "";
+          }
+          return message;
+      }
+      current = current + 1;
+   }
+   free(copy);
+   free(token);
+   return message;
+}
+
+
+int send_message(int fd, char image_path[], char head[]){
+   
+    struct stat stat_buf;  /* hold information about input file */
+
+    write(fd, head, strlen(head));
+
+    int fdimg = open(image_path, O_RDONLY);
+    
+    if(fdimg < 0){
+        printf("Cannot Open file path : %s with error %d\n", image_path, fdimg); 
+    }
+     
+    fstat(fdimg, &stat_buf);
+    int img_total_size = stat_buf.st_size;
+    int block_size = stat_buf.st_blksize;
+    if(fdimg >= 0){
+        ssize_t sent_size;
+
+        while(img_total_size > 0){
+              int send_bytes = ((img_total_size < block_size) ? img_total_size : block_size);
+              int done_bytes = sendfile(fd, fdimg, NULL, block_size);
+              img_total_size = img_total_size - done_bytes;
+        }
+        if(sent_size >= 0){
+            printf("send file: %s \n" , image_path);
+        }
+        close(fdimg);
+    }
+}
